4712c34644f492b28f90961e61235bd884cde10f
powerpook <powerkey@ibax.io>
2019-05-02 18:25:56 +0800

    explorer init

---
+++ daemons/ecosystemdeal.go
@@ -0,0 +1,151 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) IBAX All rights reserved.
+ *  See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+package daemons
+
+import (
+	//"fmt"
+	"context"
+	"encoding/json"
+	"sync/atomic"
+	"time"
+
+	"github.com/IBAX-io/go-explorer/models"
+	"github.com/IBAX-io/go-ibax/packages/consts"
+	"github.com/IBAX-io/go-ibax/packages/model"
+
+	"github.com/IBAX-io/go-explorer/services"
+	//"github.com/IBAX-io/go-explorer/models"
+	log "github.com/sirupsen/logrus"
+)
+
+//recvice websocket request sendTopData
+func EcosystemDealupdate(ctx context.Context) error {
+	//bk := &models.TransactionStatus{}
+	services.Deal_Redis_Dashboard()
+	services.SendWebsocketData = make(chan bool, 1)
+	for {
+		select {
+		case <-ctx.Done():
+			log.Error("NodeTranStatusSumupdate done his work")
+			return nil
+		case <-models.GetScanOut:
+			if err := models.GetScanOutDataToRedis(); err != nil {
+				log.Info("GetScanOutDataToRedis failed:", err)
+			}
+			err := services.WorkDealBlock()
+			if err != nil {
+				log.Info("WorkDealBlock", err)
+			}
+		}
+	}
+}
+
+func Sys_Work_ChainValidBlock(ctx context.Context) {
+	err := ChainValidBlock()
+	if err != nil {
+		log.Info("first Sys_Work_ChainValidBlock")
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Info("Sys_Work_ChainValidBlock done his work")
+			return
+		case <-time.After(time.Second * 4):
+			err := ChainValidBlock()
+			if err != nil {
+				log.Info("ChainValidBlock")
+			}
+		}
+	}
+}
+
+var bgOnChaimRun uint32
+
+func ChainValidBlock() error {
+	if atomic.CompareAndSwapUint32(&bgOnChaimRun, 0, 1) {
+		defer atomic.StoreUint32(&bgOnChaimRun, 0)
+	} else {
+		return nil
+	}
+
+	var cf model.Confirmation
+	var bc models.BlockID
+	f, err := cf.GetGoodBlockLast()
+	if err != nil {
+		return err
+	}
+	if f {
+		fc, err := bc.GetbyName(consts.ChainMax)
+		if err != nil {
+			if err.Error() == "redis: nil" || err.Error() == "EOF" {
+			} else {
+				return err
+			}
+		}
+		if !fc {
+			bc.Time = cf.Time
+			bc.Name = consts.ChainMax
+			bc.ID = cf.BlockID
+			return bc.InsertRedis()
+		}
+		if cf.BlockID > bc.ID {
+			bc.ID = cf.BlockID
+			bc.Time = cf.Time
+			return bc.InsertRedis()
+		}
+	}
+
+	return nil
+}
+
+func Sys_CentrifugoWork(ctx context.Context) {
+	models.SendScanOut = make(chan bool, 1)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-models.SendScanOut:
+			var scanout models.ScanOut
+			rets, err := scanout.GetRedisdashboard()
+			if err != nil {
+				log.Info("GetRedisdashboard dashboard", err.Error())
+			} else {
+				if err := SendtoWebsocket(rets, &scanout); err != nil {
+					log.Info("SendtoWebsocket dashboard", err.Error())
+				}
+			}
+		}
+	}
+}
+
+func SendtoWebsocket(rets *models.ScanOutRet, scanout *models.ScanOut) error {
+
+	data, _ := json.Marshal(rets)
+	err := services.WriteChannelByte(services.ChannelDashboard, data)
+	if err != nil {
+		return err
+	}
+
+	//trans, err := scanout.GetBlockTransactions(15)
+	//if err != nil {
+	//	log.Info("GetBlockTransactions", err.Error())
+	//	return err
+	//} else {
+	//	ds, err := json.Marshal(trans)
+	//	if err != nil {
+	//		log.Info("json.Marshal", err.Error())
+	//		return err
+	//	} else {
+	//		err := services.WriteChannelByte(services.ChannelBlockAndTxsList, ds)
+	//		if err != nil {
+	//			log.Info("WriteChannelByte blocktransactionlist", err.Error())
+	//			return err
+	//		}
+	//	}
+	//}
+
+	return nil
+}
---
+++ models/nodedb.go
@@ -0,0 +1,270 @@
+package models
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/IBAX-io/go-explorer/conf"
+	"github.com/IBAX-io/go-explorer/storage"
+
+	log "github.com/sirupsen/logrus"
+	"gorm.io/gorm"
+)
+
+var nodeErr = errors.New("node params error")
+
+func GetNodeRows(node int64, tableName string) (int64, error) {
+	var count int64
+
+	db := GetNodedb(node)
+	if db != nil {
+		err := db.Table(tableName).Count(&count).Error
+		if err == gorm.ErrRecordNotFound {
+			return 0, nil
+		}
+		if err != nil {
+			return 0, err
+		}
+	}
+	return count, nil
+}
+
+func GetNodeTables(node int64) (int64, error) {
+	var count int64
+	db := GetNodedb(node)
+	if db != nil {
+		err := db.Raw("SELECT count(*) FROM pg_tables WHERE schemaname='public'").Count(&count).Error
+		if err != nil {
+			return 0, err
+		}
+	}
+	return count, nil
+}
+
+func GetNodeAllColumnTypes(node int64, tblname string) ([]map[string]string, error) {
+	return GetAll(node, `SELECT column_name, data_type,column_default 
+		FROM information_schema.columns
+		WHERE table_name = ?
+		ORDER BY ordinal_position ASC`, -1, tblname)
+}
+
+func GetNodeTableOrder(order, tablname string) string {
+	defaultorder := " asc"
+	if order != "" {
+		if strings.Contains(order, "desc") || strings.Contains(order, "DESC") {
+			defaultorder = "desc"
+		}
+	}
+
+	switch tablname {
+	case "confirmations", "info_block":
+		order = "block_id " + defaultorder
+		break
+	case "stop_daemons":
+		order = "stop_time " + defaultorder
+		break
+	case "install":
+		order = "progress " + defaultorder
+		break
+	case "transactions", "queue_tx", "log_transactions", "queue_blocks", "transactions_status", "transactions_attempts":
+		order = "hash " + defaultorder
+		break
+	}
+
+	return order
+}
+
+func GetNodedb(node int64) *gorm.DB {
+	FullNodedb := conf.GetFullNodesDbConn()
+	dlen := len(FullNodedb)
+	for i := 0; i < dlen; i++ {
+		if int64(dlen) == node {
+			node = node - 1
+		}
+		if node == FullNodedb[i].NodePosition {
+			if FullNodedb[i].Enable {
+				return FullNodedb[i].DBConn
+			} else {
+				db, err := storage.GormDBInit(FullNodedb[i].Engine, FullNodedb[i].Connect)
+				if err != nil {
+					//return err
+					log.Info("node dblink err", FullNodedb[i].NodeName, FullNodedb[i].NodePosition)
+					FullNodedb[i].Enable = false
+					FullNodedb[i].DBConn = nil
+				} else {
+					log.Info("node dblink ok", FullNodedb[i].NodeName, FullNodedb[i].NodePosition)
+					FullNodedb[i].Enable = true
+					FullNodedb[i].DBConn = db
+					return FullNodedb[i].DBConn
+				}
+			}
+		}
+
+	rs, err := GetAll(node, fmt.Sprintf(`SELECT tablename FROM pg_tables WHERE schemaname='public' and tablename like %s order by %s offset %d`, "'%"+table+"%'", order, (ids-1)*icount), int(icount))
+	return count, rs, err
+}
+
+// GetAll returns all transaction
+func GetAll(node int64, query string, countRows int, args ...interface{}) ([]map[string]string, error) {
+	db := GetNodedb(node)
+	if db != nil {
+		return GetAllTransaction(db, query, countRows, args)
+	}
+
+	return nil, nodeErr
+}
+
+// GetAllTransaction is retrieve all query result rows
+func GetAllTransaction(db *gorm.DB, query string, countRows int, args ...interface{}) ([]map[string]string, error) {
+	var result []map[string]string
+	rows, err := db.Raw(query, args...).Rows()
+	if err != nil {
+		return result, fmt.Errorf("%s in query %s %s", err, query, args)
+	}
+	defer rows.Close()
+
+	// Get column names
+	columns, err := rows.Columns()
+	//columntypes, err1 := rows.ColumnTypes();
+	if err != nil {
+		return result, fmt.Errorf("%s in query %s %s", err, query, args)
+	}
+
+	columntypes, err1 := rows.ColumnTypes()
+	if err1 != nil {
+		return result, fmt.Errorf("%s in query %s %s", err1, query, args)
+	}
+	// Make a slice for the values
+	values := make([][]byte /*sql.RawBytes*/, len(columns))
+
+	// rows.Scan wants '[]interface{}' as an argument, so we must copy the
+	// references into such a slice
+	// See http://code.google.com/p/go-wiki/wiki/InterfaceSlice for details
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	r := 0
+	// Fetch rows
+	for rows.Next() {
+		// get RawBytes from data
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return result, fmt.Errorf("%s in query %s %s", err, query, args)
+		}
+
+		// Now do something with the data.
+		// Here we just print each column as a string.
+		var value string
+		rez := make(map[string]string)
+		for i, col := range values {
+			// Here we can check if the value is nil (NULL value)
+			if col == nil {
+				value = "NULL"
+			} else {
+				if columntypes[i].DatabaseTypeName() == "BYTEA" {
+					value = hex.EncodeToString(col)
+				} else {
+					value = string(col)
+				}
+			}
+			rez[columns[i]] = value
+		}
+		result = append(result, rez)
+		r++
+		if countRows != -1 && r >= countRows {
+			break
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return result, fmt.Errorf("%s in query %s %s", err, query, args)
+	}
+	return result, nil
+}
+
+func GetQueryexec(db *gorm.DB, query string, countRows int, args ...interface{}) (*[]map[string]string, error) {
+	var result []map[string]string
+	rows, err := db.Raw(query, args...).Rows()
+	if err != nil {
+		return &result, fmt.Errorf("%s in query %s %s", err, query, args)
+	}
+	defer rows.Close()
+
+	// Get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		return &result, fmt.Errorf("%s in query %s %s", err, query, args)
+	}
+
+	columntypes, err1 := rows.ColumnTypes()
+	if err1 != nil {
+		return &result, fmt.Errorf("%s in query %s %s", err1, query, args)
+	}
+	// Make a slice for the values
+	values := make([][]byte /*sql.RawBytes*/, len(columns))
+
+	// rows.Scan wants '[]interface{}' as an argument, so we must copy the
+	// references into such a slice
+	// See http://code.google.com/p/go-wiki/wiki/InterfaceSlice for details
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	r := 0
+	// Fetch rows
+	for rows.Next() {
+		// get RawBytes from data
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			return &result, fmt.Errorf("%s in query %s %s", err, query, args)
+		}
+
+		// Now do something with the data.
+		// Here we just print each column as a string.
+		var value string
+		rez := make(map[string]string)
+		for i, col := range values {
+			// Here we can check if the value is nil (NULL value)
+			if col == nil {
+				value = "NULL"
+			} else {
+				if columntypes[i].DatabaseTypeName() == "BYTEA" {
+					value = hex.EncodeToString(col)
+				} else {
+					value = string(col)
+				}
+				//value = string(col)
+			}
+			rez[columns[i]] = value
+		}
+		result = append(result, rez)
+		r++
+		if countRows != -1 && r >= countRows {
+			break
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return &result, fmt.Errorf("%s in query %s %s", err, query, args)
+	}
+	return &result, nil
+}
+
+// GetDB is returning gorm.DB
+func GetDB(db *DbTransaction) *gorm.DB {
+	if db != nil && db.conn != nil {
+		return db.conn
+	}
+	return conf.GetDbConn().Conn()
+}
+
+func GetsqliteALLTable() (*[]map[string]string, error) {
+	return GetQueryexec(conf.GetDbConn().Conn(), `select name from sqlite_master where type='table' order by name`, -1)
+}
+
+func GetsqliteblcokALLTable() (*[]map[string]string, error) {
+	return GetQueryexec(conf.GetDbConn().Conn(), `select name from sqlite_master where type='table' order by name`, -1)
+}
