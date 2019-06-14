b6fb28b01e58a363121ec2e9f574b4024ad09333
powerpook <powerkey@ibax.io>
2019-06-14 10:31:12 +0800

    Merge branch 'master' into explorer

--- cmd/version.go
+++ cmd/version.go
@@ -10,15 +10,7 @@ import (
 
 	"github.com/IBAX-io/go-ibax/packages/consts"
 
-	"github.com/spf13/cobra"
-)
-
-// versionCmd represents the version command
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Show version",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(strings.TrimSpace(strings.Join([]string{
 			consts.VERSION, consts.BuildInfo}, " ",
 		)))
 	},
+}
