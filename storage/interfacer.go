47f7aedb798509db604404331608732cad30f3f7
powerpook <powerkey@ibax.io>
2020-10-15 20:14:32 +0800

    add view table

--- storage/interfacer.go
+++ storage/interfacer.go
@@ -2,23 +2,18 @@ package storage
 
 type DbConner interface {
 	Initer() error
+	Close() error
+}
+
+// TransactionStatus is model
+type TransactionStatus struct {
+	Hash      []byte `gorm:"primary_key;not null"  json:"hash"`
 	Time      int64  `gorm:"not null" json:"time"`
 	Type      int64  `gorm:"not null"  json:"type"`
 	Ecosystem int64  `gorm:"not null"  json:"ecosystem"`
 	WalletID  int64  `gorm:"not null"  json:"wallet_id"`
 	BlockID   int64  `gorm:"not null;index:tsblockid_idx"  json:"block_id"`
 	Error     string `gorm:"not null"  json:"error"`
-	Penalty   int64  `gorm:"not null"  json:"penalty"`
-}
-
-type BlockTxDetailedInfoHex struct {
-	BlockID      int64  `gorm:"not null;index:blockid_idx" json:"block_id"`
-	Hash         string `gorm:"primary_key;not null" json:"hash"`
-	ContractName string `gorm:"not null" json:"contract_name"`
-	//Params       map[string]interface{} `json:"params"`
-	Params string `gorm:"not null" json:"params"`
-	KeyID  string `gorm:"not null" json:"key_id"`
-	Time   int64  `gorm:"not null" json:"time"`
 	Type   int64  `gorm:"not null" json:"type"`
 	Size   int64  `gorm:"not null" json:"size"`
 
