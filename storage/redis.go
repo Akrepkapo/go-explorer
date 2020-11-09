68aef4265c5d3ed70aedb18c891fa8607eff7852
powerpook <powerkey@ibax.io>
2020-11-09 18:36:47 +0800

    update  mint_count app_param, add foundation_group 2

--- storage/redis.go
+++ storage/redis.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/go-redis/redis/v8"
+)
+
+var rc *redis.Client
+var ctx = context.Background()
 
 type RedisModel struct {
 	Address  string `yaml:"address"`
@@ -14,22 +19,6 @@ type RedisModel struct {
 func (r *RedisModel) Str() string {
 	return fmt.Sprintf("%s:%d", r.Address, r.Port)
 }
-
-func (r *RedisModel) Initer() error {
-	rc = redis.NewClient(&redis.Options{
-		Addr:     r.Str(),
-		Password: r.Password,
-		DB:       0,
-	})
-	_, err := rc.Ping(ctx).Result()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (r *RedisModel) Conn() *redis.Client {
-	return rc
 }
 func (l *RedisModel) Close() error {
 	return rc.Close()
